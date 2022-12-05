1b2821c8eff7407299412aaabd3e6a497ca605e4
Alex N <xander.nesta@gmail.com>
2022-12-05 16:41:16 -0500

    setup webpack and npm modules and configs

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # odin-todo
+### Todo List project for [the Odin Project](https://www.theodinproject.com/lessons/node-path-javascript-todo-list)
+[Live version here](https://xandernesta.github.io/odin-todo/)
+
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        publicPath: '/',
+        },
+    devServer: {
+        static: './dist',
+        host: '192.168.99.190', // in this section, I have tried to change IP address by server IP
+        port: 3000
+        },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            title: 'Todo List',
+            //content: 'class="content"',
+            //template: './src/template_index.html',
+            filename: './index.html'
+        }),
+        ],  
+    module: {
+        rules: [
+            {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+            },
+        ],
+        },
+    optimization: {
+        runtimeChunk: 'single',
+        },
+};
