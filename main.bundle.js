5c77713de4efeae8151f57fd7b5127e4317a0c36
Alex N <xander.nesta@gmail.com>
2022-12-07 14:35:07 -0500

    add some foundation elements and styles

--- main.bundle.js
+++ main.bundle.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
@@ -8,13 +9,23 @@
  */
 (self["webpackChunk"] = self["webpackChunk"] || []).push([["main"],{
 
+/***/ "./src/UI.js":
+/*!*******************!*\
+  !*** ./src/UI.js ***!
+  \*******************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n//import Project\n//import Task\n//import format from date-fns\n\n//DOM elements\n//event listeners\n\nconst dom = (() => {\n\n    const header = (() => {\n            const header = document.createElement('header');\n            header.classList.add('header');\n            const todoIcon = document.createElement('i');\n            todoIcon.classList.add('fa-regular');\n            todoIcon.classList.add('fa-list-check');\n            const title = document.createElement('h1');\n            title.textContent = 'To-Do';\n            title.classList.add('title');\n            title.appendChild(todoIcon)\n            header.appendChild(title);\n        \n            // return header;\n        \n    })();\n\n})();\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (dom);\n\n//# sourceURL=webpack:///./src/UI.js?");
+
+/***/ }),
+
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("alert(\"I'm alive!\");\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _UI_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./UI.js */ \"./src/UI.js\");\n\n\ndocument.addEventListener('DOMContentLoaded', _UI_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"]);\n//event fires when the HTML document has been completely parsed, and all deferred scripts (<script defer src=\"…\"> and <script type=\"module\">) have downloaded and executed. It doesn't wait for other things like images, subframes, and async scripts to finish loading.\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ })
 
